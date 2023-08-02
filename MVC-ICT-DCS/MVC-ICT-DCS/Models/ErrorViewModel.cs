6246661d772b7cf7f275235999de21e6a6ab6929
Nicholas Atkins <37705884+Nicholas-O-Atkins@users.noreply.github.com>
2023-08-02 13:30:19 -0230

    Restarting with proper template

---
+++ MVC-ICT-DCS/MVC-ICT-DCS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_ICT_DCS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
