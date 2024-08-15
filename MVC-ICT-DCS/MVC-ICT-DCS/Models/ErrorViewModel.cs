bfab3cedb09abbeca1b1009037a0b1463cc373f6
Nicholas Atkins <37705884+Nicholas-O-Atkins@users.noreply.github.com>
2024-08-15 15:15:10 -0230

    Removing old stuff

--- MVC-ICT-DCS/MVC-ICT-DCS/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVC_ICT_DCS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
