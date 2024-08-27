edcbb897ef9d5ebee379af729b81d6333e8541e4
Nicholas Atkins <37705884+Nicholas-O-Atkins@users.noreply.github.com>
2024-08-27 17:52:40 -0230

    Working on a Seed for the database
    Probably wont post till I finalize the seed methods

---
+++ WarframeLeftoverAPI/WarframeLeftoverAPI/Seed.cs
@@ -0,0 +1,21 @@
+﻿using WarframeLeftoverAPI.Data;
+using WarframeLeftoverAPI.Models;
+
+namespace WarframeLeftoverAPI
+{
+    public class Seed
+    {
+        private readonly DataContext dataContext;
+        public Seed(DataContext context)
+        {
+            this.dataContext = context;
+        }
+        public void SeedDataContext()
+        {
+            if (!dataContext.Tenno_Task_Items.Any())
+            {
+
+            }
+        }
+    }
+}
